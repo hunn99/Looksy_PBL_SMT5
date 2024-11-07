c917edc98c3ac7acdb1a448bb355dfb62b5d2323
satriaabraarr <satria.abrarr@gmail.com>
2024-11-07 18:14:03 +0700

    registration page

--- lib/presentation/pages/onboarding/registration.dart
+++ lib/presentation/pages/onboarding/registration.dart
@@ -1,12 +1,244 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:looksy/presentation/utils/theme.dart';
+import 'package:looksy/presentation/widgets/button/button.dart';
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
+
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+  final FocusNode _passwordFocusNode = FocusNode();
+  final FocusNode _confirmPasswordFocusNode = FocusNode();
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordVisible = !_isPasswordVisible;
+    });
+  }
+
+  void _toggleConfirmPasswordVisibility() {
+    setState(() {
+      _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+    });
+  }
+
+  void _unfocus() {
+    FocusScope.of(context).unfocus();
+  }
+
+  @override
+  void dispose() {
+    _passwordFocusNode.dispose();
+    _confirmPasswordFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: Center(child: Text('Registration'))),
+      child: Scaffold(
+        body: GestureDetector(
+          onTap: _unfocus, // Menghilangkan fokus dari TextField saat area luar diketuk
+          child: SingleChildScrollView(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 64),
+                  Image.asset(
+                    'assets/images/Logo_black.png',
+                    height: 40,
+                  ),
+                  const SizedBox(height: 32),
+                  const Text(
+                    'Welcome to Looksy!',
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Color(0xFF1b1b1b),
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -2,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    'Discover your perfect hairstyle tailored to your\n'
+                    'unique face shape. Let\'s get started on your style\n'
+                    'journey!',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFFB0B0B0),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  TextField(
+                    style: const TextStyle(fontSize: 11),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 16,
+                      ),
+                      hintText: 'Email',
+                      hintStyle: const TextStyle(color: Color(0xFFD1D1D1), fontSize: 16),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFFE7E7E7)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFF1b1b1b)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    style: const TextStyle(fontSize: 11),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 16,
+                      ),
+                      hintText: 'Username',
+                      hintStyle: const TextStyle(color: Color(0xFFD1D1D1), fontSize: 16),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFFE7E7E7)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFF1b1b1b)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    style: const TextStyle(fontSize: 11),
+                    focusNode: _passwordFocusNode,
+                    obscureText: !_isPasswordVisible,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 16,
+                      ),
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(color: Color(0xFFD1D1D1), fontSize: 16),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFFE7E7E7)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFF1b1b1b)),
+                      ),
+                      suffixIcon: GestureDetector(
+                        onTap: _togglePasswordVisibility,
+                        child: Icon(
+                          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    style: const TextStyle(fontSize: 11),
+                    focusNode: _confirmPasswordFocusNode,
+                    obscureText: !_isConfirmPasswordVisible,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 16,
+                      ),
+                      hintText: 'Confirm Password',
+                      hintStyle: const TextStyle(color: Color(0xFFD1D1D1), fontSize: 16),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFFE7E7E7)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: Color(0xFF1b1b1b)),
+                      ),
+                      suffixIcon: GestureDetector(
+                        onTap: _toggleConfirmPasswordVisibility,
+                        child: Icon(
+                          _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 80),
+                  Button(
+                    label: 'Sign up',
+                    onTap: () {
+                      // Add action for registration
+                    },
+                    isDisabled: false,
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                    textSize: 16,
+                    colorText: Colors.white,
+                    colorBackground: Color(0xFF1b1b1b),
+                  ),
+                  const SizedBox(height: 12),
+                  Button(
+                    label: 'Sign up with Google',
+                    onTap: () {
+                      // Add action for Google sign up
+                    },
+                    isDisabled: false,
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                    textSize: 16,
+                    colorText: Color(0xFF1b1b1b),
+                    colorBackground: Colors.white,
+                    borderColor: Color(0xFF1b1b1b),
+                    icon: Container(
+                      height: 20,
+                      width: 20,
+                      child: Image.asset(
+                        'assets/images/google.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Already have an account? ",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF1b1b1b),
+                          ), // Ukuran font diubah menjadi 11
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          context.push('/login');
+                        },
+                        child: const Text(
+                          'Log in',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16, // Ukuran font diubah menjadi 11
+                            color: Color(0xFF1b1b1b),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/presentation/widgets/button/button.dart
+++ lib/presentation/widgets/button/button.dart
@@ -7,6 +7,10 @@ class Button extends StatelessWidget {
   final bool isDisabled;
   final Color? colorText;
   final Color? colorBackground;
+  final EdgeInsetsGeometry? padding; // Tambahkan padding
+  final double textSize; // Tambahkan textSize
+  final Color? borderColor; // Tambahkan borderColor
+  final Widget? icon; // Tambahkan icon
 
   const Button({
     super.key,
@@ -15,6 +19,10 @@ class Button extends StatelessWidget {
     this.isDisabled = false,
     required this.colorText,
     required this.colorBackground,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16),
+    this.textSize = 16,
+    this.borderColor, 
+    this.icon, 
   });
 
   @override
@@ -22,20 +30,25 @@ class Button extends StatelessWidget {
     return GestureDetector(
       onTap: isDisabled ? null : onTap,
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: padding,
         height: 48,
         decoration: BoxDecoration(
           color: isDisabled ? neutralTheme[300] : colorBackground,
           borderRadius: BorderRadius.circular(100),
+          border: borderColor != null ? Border.all(color: borderColor!) : null,
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            if (icon != null) ...[
+              icon!,
+              const SizedBox(width: 8),
+            ],
             Text(
               label,
               style: TextStyle(
-                  fontSize: 16,
+                  fontSize: textSize,
                   fontWeight: FontWeight.w500,
                   color: isDisabled ? neutralTheme[500] : colorText),
             ),
